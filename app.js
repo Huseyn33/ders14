8490bfbd8e2f61da0547de386a384e351eabdd6b
Huseyn33 <huseynoyunkanali1@gmail.com>
2025-04-03 19:35:54 +0400

    finish

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+
+let user=prompt("adiniz nedir");
+console.log(user);
+let yas=prompt("yasiniz necedir");
+console.log(yas)
+let sevimlireng=prompt("sevimli renginiz nedir");
+console.log(sevimlireng)
+let ensevdigifealiyyet=prompt("en sevdiginiz fealiyyet");
+console.log(ensevdigifealiyyet)
